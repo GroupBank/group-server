310b54d1f3a07b7bc21248c95bc5c65b40345a26
David Fialho <fialho.david@protonmail.com>
2017-12-02 20:12:50 +0000

    Add decorators 'authenticate' and 'post_parameters'

---
+++ rest/decorators.py
@@ -0,0 +1,62 @@
+""" Module contains a set of decorators for the views """
+
+from functools import wraps
+
+from django.http import HttpResponseBadRequest, HttpResponseForbidden
+
+import common.crypto.ec_secp256k1 as crypto
+
+
+def post_parameters(view):
+    """
+    Decorator for all views.
+
+    Extracts from a POST request the three parameters included in all messages/requests:
+    *author*, *payload*, and *signature*. It passes these parameters as arguments to the view,
+    along with the original request.
+
+    It responds with a bad request if one of the parameters is missing from the request.
+    """
+
+    @wraps(view)
+    def wrapper(request):
+
+        try:
+            author = request.POST['author']
+            payload = request.POST['payload']
+            signature = request.POST['signature']
+        except KeyError:
+            return HttpResponseBadRequest("Malformed request: request is missing a parameter.")
+
+        # Call the view outside of the try/except to avoid unexpected results if the
+        # view raises a KeyError
+        return view(request, author, payload, signature)
+
+    return wrapper
+
+
+def authenticate(view):
+    """
+    Decorator for all views.
+
+    Authenticates the request. It verifies if the signature for the payload corresponds to the
+    author's key.
+
+    It responds with a Forbidden error if the author key is not valid or if the signature is not
+    verified.
+    """
+
+    @wraps(view)
+    def wrapper(request, author, payload, signature):
+        try:
+            crypto.verify(author, signature, payload)
+        except crypto.InvalidSignature:
+            return HttpResponseForbidden("Authentication failed: signature was not validated.")
+        except crypto.InvalidKey:
+            return HttpResponseForbidden("Authentication failed: key is invalid.")
+
+        # Call the view outside of the try/except to avoid unexpected results if the
+        # view raises a and InvalidSignature or InvalidKey
+        return view(request, author, payload, signature)
+
+    return wrapper
---
+++ rest/test_decorators.py
@@ -0,0 +1,119 @@
+""" Module containing the tests for the 'decorators' module """
+
+from unittest.mock import MagicMock
+
+import pytest
+from django.http import HttpResponseBadRequest, HttpResponseForbidden
+from hamcrest import assert_that, is_, instance_of
+
+import common.crypto.ec_secp256k1 as crypto
+from rest.decorators import post_parameters, authenticate
+
+
+class TestPostParameters:
+    """ Test cases for the post_parameters() decorator """
+
+    @pytest.mark.parametrize("parameters", [
+        {
+            'author': '#1',
+            'payload': '{"params": "value"}',
+            'signature': 'asjkdasjkd',
+        },
+        {
+            'author': '#1',
+            'payload': '{"params": "value"}',
+            'signature': 'asjkdasjkd',
+            'other': 'value',
+        },
+    ])
+    def test_RequestWithAllParameters_CallsTheViewWithNoErrors(self, parameters):
+        view = MagicMock()
+        request = MagicMock()
+        request.POST = {
+            'author': '#1',
+            'payload': '{"params": "value"}',
+            'signature': 'asjkdasjkd',
+        }
+
+        # Inject the view into the decorator and call the wrapper
+        post_parameters(view)(request)
+
+        view.assert_called_once_with(request, '#1', '{"params": "value"}', 'asjkdasjkd')
+
+    @pytest.mark.parametrize("parameters", [
+        {
+            'payload': '{"params": "value"}',
+            'signature': 'asjkdasjkd',
+        },
+        {
+            'author': '#1',
+            'signature': 'asjkdasjkd',
+        },
+        {
+            'author': '#1',
+            'payload': '{"params": "value"}',
+        },
+        {},
+    ])
+    def test_RequestMissingSomeParameters_RespondsWithBadRequestError(self, parameters):
+        view = MagicMock()
+        request = MagicMock()
+        request.POST = parameters
+
+        # Inject the view into the decorator and call the wrapper
+        response = post_parameters(view)(request)
+
+        assert_that(response, is_(instance_of(HttpResponseBadRequest)))
+
+
+class TestAuthenticate:
+    """ Test cases for the authenticate() decorator """
+
+    def test_RequestWithValidSignature_CallsTheViewWithSameArguments(self):
+        view = MagicMock()
+        request = MagicMock()
+
+        key, pubkey = crypto.generate_keys()
+        signature = crypto.sign(key, '{"params": "value"}')
+        request.POST = {
+            'author': pubkey,
+            'payload': '{"params": "value"}',
+            'signature': signature,
+        }
+
+        # Inject the view into the decorator and call the wrapper
+        authenticate(view)(request, pubkey, '{"params": "value"}', signature)
+
+        view.assert_called_once_with(request, pubkey, '{"params": "value"}', signature)
+
+    def test_RequestWithInvalidKey_RespondsWithForbiddenError(self):
+        view = MagicMock()
+        request = MagicMock()
+        request.POST = {
+            'author': 'invalid key',
+            'payload': '{"params": "value"}',
+            'signature': 'adkasjd',
+        }
+
+        # Inject the view into the decorator and call the wrapper
+        response = authenticate(view)(request, 'invalid key', '{"params": "value"}', 'adkasjd')
+
+        assert_that(response, is_(instance_of(HttpResponseForbidden)))
+
+    def test_RequestWithInvalidSignature_RespondsWithForbiddenError(self):
+        view = MagicMock()
+        request = MagicMock()
+
+        key, pubkey = crypto.generate_keys()
+        other_key, _ = crypto.generate_keys()
+        signature = crypto.sign(other_key, '{"params": "value"}')
+        request.POST = {
+            'author': pubkey,
+            'payload': '{"params": "value"}',
+            'signature': signature,
+        }
+
+        # Inject the view into the decorator and call the wrapper
+        response = authenticate(view)(request, pubkey, '{"params": "value"}', signature)
+
+        assert_that(response, is_(instance_of(HttpResponseForbidden)))
